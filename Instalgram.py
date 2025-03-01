53a6303ac8f3b1c18d44904723b96ab044aee8ed
Rohanraj1234 <114574911+Rohanraj1234@users.noreply.github.com>
2025-03-01 13:40:44 +0530

    Update Instabot.py

--- Instalgram.py
+++ Instalgram.py
@@ -1,20 +1,36 @@
 from instabot import Bot
 bot = Bot()
+print("Hey Dear User, login your account !!")
 #login Code
-bot.login(username = "User_Account_ID",password = "password")
-# To follow Someone
-bot.follow = ("Username which you want to follow")
+bot.login(username = input("Enter Username : "),password = input("Enter Password : "))
+
+task = input("Enter the task (follow, upload photo, unfollow, message, Followers list, Followint list): ").lower()
+
+if task == "follow":
+# To follow Someone    
+    bot.follow(input("Enter Account ID Which you want to follow: "))
+
+if task == "upload photo":
 # To upload Photo 
-bot.upload_photo("path of photo in desktop", caption = "Ur Choice")
+    bot.upload_photo(input("Paste the path of image in .py form: "), caption=input("Enter Caption: "))
+
+if task == "unfollow":
 # To unfollow Someone
-bot.unfollow("Account Name")
+    bot.unfollow(input("Enter Account ID: "))
+
+if task == "message":
 # To Send message
-bot.send_message("message",["1st Account","2nd Account"])
+    bot.send_message(input("message"), [input("1st Account", "2nd Account")])
+
+if task == "Followers list":
 # Information about followers
-followers = bot.get_user_followers("User_Account_ID")
-for follower in followers:
-    print(bot.get_user_info(follower))
-# Infomation about following
-following = bot.get_user_following("User_Account_ID")
-for following in following:
-    print(bot.get_user_info(following))
+    followers = bot.get_user_followers("User_Account_ID")
+    for follower in followers:
+        print(bot.get_user_info(follower))
+
+if task == "Following List":
+# Information about following
+    following = bot.get_user_following("User_Account_ID")
+    for user in following:
+        print(bot.get_user_info(user))
+
